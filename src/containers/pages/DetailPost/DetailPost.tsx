2433e1a0af3ec1557713b9b703afbf6ce33bd647
osenbal <enbal.blogs.215@gmail.com>
2022-08-27 16:27:38 +0700

    add loading detail post

--- src/containers/pages/DetailPost/DetailPost.tsx
+++ src/containers/pages/DetailPost/DetailPost.tsx
@@ -3,14 +3,17 @@ import { Render } from '@9gustin/react-notion-render';
 import { useParams } from 'react-router-dom';
 import { GET_DETAIL_POST } from '../../../api/constant';
 import './DetailPost.css';
+import DetailPostLoading from './DetailPostLoading';
 
 function DetailPost() {
   const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     fetch(`${GET_DETAIL_POST}/${id}`, {
       method: 'GET',
       headers: {
@@ -21,12 +24,14 @@ function DetailPost() {
       .then((res) => res.json())
       .then((data) => {
         setPost(data?.data);
+        setIsLoading(false);
       })
       .catch((error) => {
         if (error.name === 'AbortError') {
           console.log('Request was aborted');
         } else {
           console.log(error);
+          setIsLoading(false);
         }
       });
 
@@ -38,7 +43,13 @@ function DetailPost() {
     <>
       <main id="detail-post">
         <div className="container">
-          <Render blocks={post} />
+          {!isLoading ? (
+            <div className="detail-post__content">
+              <Render blocks={post} />
+            </div>
+          ) : (
+            <DetailPostLoading />
+          )}
         </div>
       </main>
     </>
---
+++ src/containers/pages/DetailPost/DetailPostLoading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function DetailPostLoading() {
+  return (
+    <>
+      <main id="detail-post">
+        <div className="container">
+          <div className="detail-post__title-loading"></div>
+          <div className="detail-post__img-loading"></div>
+          <div className="detail-post__content1-loading"></div>
+          <div className="detail-post__content2-loading"></div>
+          <div className="detail-post__content3-loading"></div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default DetailPostLoading;
